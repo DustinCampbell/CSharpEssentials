75307b1a37cd8bf172c95f533f7a9309a1143fd4
Dustin Campbell <dustin@teamcampbell.org>
2015-03-12 15:45:19 -0700

    Add more unit tests and fix a broken one

--- Source/CSharpEssentials.Tests/ConvertToInterpolatedString/ConvertToInterpolatedStringRefactoringTests.cs
+++ Source/CSharpEssentials.Tests/ConvertToInterpolatedString/ConvertToInterpolatedStringRefactoringTests.cs
@@ -31,6 +31,56 @@ class C
         var s = $""{42}"";
     }
 }";
+
+            TestCodeRefactoring(markupCode, expected);
+        }
+
+        [Test]
+        public void MultipleSpecifiers()
+        {
+            const string markupCode = @"
+class C
+{
+    void M()
+    {
+        var s = [|string.Format(""{0} - {0}"", 42)|];
+    }
+}";
+
+            const string expected = @"
+class C
+{
+    void M()
+    {
+        var s = $""{42} - {42}"";
+    }
+}";
+
+            TestCodeRefactoring(markupCode, expected);
+        }
+
+        [Test]
+        public void MultipleArguments()
+        {
+            const string markupCode = @"
+class C
+{
+    void M()
+    {
+        var s = [|string.Format(""{0} + {1}"", 19, 23)|];
+    }
+}";
+
+            const string expected = @"
+class C
+{
+    void M()
+    {
+        var s = $""{19} + {23}"";
+    }
+}";
+
+            TestCodeRefactoring(markupCode, expected);
         }
 
         [Test]
